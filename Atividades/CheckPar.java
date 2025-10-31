fcec1fbd20b30ec91dbd92dc3d97aa494b011462
ogabrielcavalcante <202517640044@unifacol.edu.br>
2025-10-31 10:11:13 -0300

    adicionado atividade parouimpar

---
+++ Atividades/CheckPar.java
@@ -0,0 +1,26 @@
+package Atividades;
+
+import java.util.Scanner;
+
+public class CheckPar {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Digite um número para verificar se ele é Par ou Ímpar:");
+        
+        long numero = sc.nextLong();  // <-- use long
+        
+        if (ehPar(numero)) {
+            System.out.println("É par!");
+        } else {
+            System.out.println("É ímpar!");
+        }
+        
+        sc.close();
+    }
+    
+    public static boolean ehPar(long num) {  // <-- e aqui também
+        return num % 2 == 0;
+    }
+}
